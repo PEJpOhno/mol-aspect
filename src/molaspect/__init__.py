ef1547dd49f220c732498c0ed0656625794afb4b
PEJpOhno <pejpohn@gmail.com>
2025-07-22 21:37:24 +0900

    create src

---
+++ src/molaspect/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025 Mitsuru Ohno
+#  Use of this source code is governed by a BSD-3-style
+#  license that can be found in the LICENSE file.
+
+# 07/22/2025, M. Ohno
+# mol_aspect __init__
+
+from ._version import __version__
